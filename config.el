65870b1be7cd0ac08a9954585323d9e744d65a9e
CloudyChris <a.c.pufu@gmail.com>
2025-03-09 22:04:24 +0200

    added pinentry

--- config.el
+++ config.el
@@ -74,3 +74,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(use-package! pinentry
+	:init (setq epg-pinentry-mode `loopback)
+	      (pinentry-start))
